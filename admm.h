475416d24da4fb277ed80b5ade71d6fcecf58f3f
hanicho <hanicho1@umbc.edu>
2016-12-13 05:56:43 -0500

    Update admm.h

--- admm.h
+++ admm.h
@@ -75,4 +75,44 @@ void admm_weights(struct admm_learn *obs, struct admm_node *node, size_t l);
 void admm_update(struct admm_learn *obs, struct admm_node *node);
 void admm_train(struct admm_learn *obs, struct mlp *net, const gsl_matrix *Xt, const gsl_matrix *Yt, const gsl_matrix *Xv, const gsl_matrix *Yv);
 
+int get_cluster_memory_usage_kb(long* vmrss_per_process, long* vmsize_per_process, int root, int np) {
+     long vmrss_kb;
+     long vmsize_kb;
+     int ret_code = get_memory_usage_kb(&vmrss_kb, &vmsize_kb);
+
+     if (ret_code != 0) {
+	  printf("Could not gather memory usage!\n");
+	  return ret_code;
+     }
+
+     MPI_Gather(&vmrss_kb, 1, MPI_UNSIGNED_LONG,
+		vmrss_per_process, 1, MPI_UNSIGNED_LONG,
+		root, MPI_COMM_WORLD);
+
+     MPI_Gather(&vmsize_kb, 1, MPI_UNSIGNED_LONG,
+		vmsize_per_process, 1, MPI_UNSIGNED_LONG,
+		root, MPI_COMM_WORLD);
+
+     return 0;
+}
+
+int get_global_memory_usage_kb(long* global_vmrss, long* global_vmsize, int np) {
+     long vmrss_per_process[np];
+     long vmsize_per_process[np];
+     int ret_code = get_cluster_memory_usage_kb(vmrss_per_process, vmsize_per_process, 0, np);
+
+     if (ret_code != 0) {
+	  return ret_code;
+     }
+
+     *global_vmrss = 0;
+     *global_vmsize = 0;
+     for (int i = 0; i < np; i++) {
+	  *global_vmrss += vmrss_per_process[i];
+	  *global_vmsize += vmsize_per_process[i];
+     }
+
+     return 0;
+}
+
 #endif
